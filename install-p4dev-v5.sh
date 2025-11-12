569193e8ef81af50642300c8e6f7af49f2d5013c
Garegin Grigoryan <mr.garegin.grigoryan@gmail.com>
2025-11-12 10:54:43 -0500

    Add files via upload

---
+++ install-p4dev-v5.sh
@@ -0,0 +1,321 @@
+#! /bin/bash
+
+# Copyright 2020 Intel Corporation
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# This script differs from the original script in https://github.com/jafingerhut/p4-guide/blob/master/bin/install-p4dev-v5.sh
+# Modifications are added due to expired keys
+
+
+# This script differs from install-p4dev-v4.sh as follows:
+
+# It installs Ubuntu pre-compiled packages created by scripts and
+# published by Radostin Stoyanov, with scripts for creating them here:
+
+# https://github.com/p4lang/p4pi/tree/master/packages
+
+# Remember the current directory when the script was started:
+INSTALL_DIR="${PWD}"
+
+THIS_SCRIPT_FILE_MAYBE_RELATIVE="$0"
+THIS_SCRIPT_DIR_MAYBE_RELATIVE="${THIS_SCRIPT_FILE_MAYBE_RELATIVE%/*}"
+THIS_SCRIPT_DIR_ABSOLUTE=`readlink -f "${THIS_SCRIPT_DIR_MAYBE_RELATIVE}"`
+
+ubuntu_version_warning() {
+    1>&2 echo "This software has only been tested on Ubuntu 20.04"
+    1>&2 echo ""
+    1>&2 echo "Proceed installing manually at your own risk of"
+    1>&2 echo "significant time spent figuring out how to make it all"
+    1>&2 echo "work, or consider getting VirtualBox and creating an"
+    1>&2 echo "Ubuntu virtual machine with one of the tested versions."
+}
+
+abort_script=0
+
+lsb_release >& /dev/null
+if [ $? != 0 ]
+then
+    1>&2 echo "No 'lsb_release' found in your command path."
+    ubuntu_version_warning
+    exit 1
+fi
+
+distributor_id=`lsb_release -si`
+ubuntu_release=`lsb_release -s -r`
+if [ "${distributor_id}" = "Ubuntu" -a \( "${ubuntu_release}" = "20.04" \) ]
+then
+    echo "Found distributor '${distributor_id}' release '${ubuntu_release}'.  Continuing with installation."
+else
+    ubuntu_version_warning
+    1>&2 echo ""
+    1>&2 echo "Here is what command 'lsb_release -a' shows this OS to be:"
+    lsb_release -a
+    exit 1
+fi
+
+# Minimum required system memory is 2 GBytes, minus a few MBytes
+# because from experiments I have run on several different Ubuntu
+# Linux VMs, when you configure them with 2 Gbytes of RAM, the first
+# line of /proc/meminfo shows a little less than that available, I
+# believe because some memory occupied by the kernel is not shown.
+
+min_mem_MBytes=`expr 2 \* \( 1024 - 64 \)`
+memtotal_KBytes=`head -n 1 /proc/meminfo | awk '{print $2;}'`
+memtotal_MBytes=`expr ${memtotal_KBytes} / 1024`
+
+if [ "${memtotal_MBytes}" -lt "${min_mem_MBytes}" ]
+then
+    memtotal_comment="too low"
+    abort_script=1
+else
+    memtotal_comment="enough"
+fi
+
+echo "Minimum recommended memory to run this script: ${min_mem_MBytes} MBytes"
+echo "Memory on this system from /proc/meminfo:      ${memtotal_MBytes} MBytes -> $memtotal_comment"
+
+# In my testing on 2021-Nov-14, it took about 1.8 GBytes of disk space
+# to run this script.  Check for at least 2.5 GBytes free to leave
+# some room for future growth in the size of the packages.
+min_free_disk_MBytes=`expr 2 \* 1024 + 512`
+free_disk_MBytes=`df --output=avail --block-size=1M . | tail -n 1`
+
+if [ "${free_disk_MBytes}" -lt "${min_free_disk_MBytes}" ]
+then
+    free_disk_comment="too low"
+    abort_script=1
+else
+    free_disk_comment="enough"
+fi
+
+echo "Minimum free disk space to run this script:    ${min_free_disk_MBytes} MBytes"
+echo "Free disk space on this system from df output: ${free_disk_MBytes} MBytes -> $free_disk_comment"
+
+if [ "${abort_script}" == 1 ]
+then
+    echo ""
+    echo "Aborting script because system has too little RAM or free disk space"
+    exit 1
+fi
+
+PATCH_DIR="${THIS_SCRIPT_DIR_ABSOLUTE}/patches"
+
+for dir in "${PATCH_DIR}"
+do
+    if [ -d "${dir}" ]
+    then
+	echo "Found directory containing patches: ${dir}"
+    else
+	echo "NO directory containing patches: ${dir}"
+	abort_script=1
+    fi
+done
+
+if [ "${abort_script}" == 1 ]
+then
+    echo ""
+    echo "Aborting script because an expected directory does not exist (see above)."
+    echo ""
+    echo "This script is not designed to work if you only copy the"
+    echo "script file to a system.  You should run it in the context"
+    echo "of a cloned copy of the repository: https://github/jafingerhut/p4-guide"
+    exit 1
+fi
+
+echo "Passed all sanity checks"
+
+set -e
+set -x
+
+set +x
+echo "This script installs pre-compiled Debian packages of the P4_16 (and"
+echo "also P4_14) compiler, and the behavioral-model software packet"
+echo "forwarding program, that can behave as just about any legal P4"
+echo "program."
+echo ""
+echo "It is regularly tested on freshly installed Ubuntu 20.04 systems,"
+echo "with all Ubuntu software updates as of the date of"
+echo "testing.  See this directory for log files recording the last"
+echo "date this script was tested on its supported operating systems:"
+echo ""
+echo "    https://github.com/jafingerhut/p4-guide/tree/master/bin/output"
+echo ""
+echo "As of 2021-Nov-14, the files installed by this script consume about 2"
+echo "GB of disk space.  Running the script downloads about 250 MBytes of"
+echo "data from the Internet."
+echo ""
+echo "On a 2015 MacBook Pro with a decent speed Internet connection"
+echo "and an SSD drive, running Ubuntu Linux in a VirtualBox VM, it"
+echo "took about 3 minutes."
+echo ""
+
+echo "------------------------------------------------------------"
+echo "Time and disk space used before installation begins:"
+set -x
+date
+df -h .
+df -BM .
+
+# Check to see which versions of Python-related programs this system
+# already has installed, before the script starts installing things.
+lsb_release -a
+python -V  || echo "No such command in PATH: python"
+python2 -V || echo "No such command in PATH: python2"
+python3 -V || echo "No such command in PATH: python3"
+pip -V  || echo "No such command in PATH: pip"
+pip2 -V || echo "No such command in PATH: pip2"
+pip3 -V || echo "No such command in PATH: pip3"
+
+sudo apt-get install -qq -y --no-install-recommends --fix-missing\
+  curl \
+  git \
+  wget \
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Add repository with P4 packages
+# https://build.opensuse.org/project/show/home:p4lang
+
+echo "deb http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
+wget -qO - "http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/Release.key" | sudo apt-key add -
+
+set -e
+
+REPO_FILE="/etc/apt/sources.list.d/home:p4lang.list"
+
+sudo bash -c "cat > $REPO_FILE <<EOF
+deb [trusted=yes] http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_20.04/ /
+EOF"
+
+sudo apt-key del A4666B6753C0F7D1 2>/dev/null || true
+sudo apt-get update -qq
+
+sudo apt-get install -qq -y --no-install-recommends --fix-missing\
+  iproute2 \
+  net-tools \
+  python3 \
+  python3-pip \
+  tcpdump \
+  unzip \
+  vim \
+  xterm \
+  p4lang-p4c \
+  p4lang-bmv2 \
+  p4lang-pi
+
+pip -V  || echo "No such command in PATH: pip"
+pip2 -V || echo "No such command in PATH: pip2"
+pip3 -V || echo "No such command in PATH: pip3"
+# At multiple points I do a 'pip list' command.  This is not required
+# for a successful installation -- I do it mainly because I am curious
+# to see in the log output files from running this script what
+# packages and versions were installed at those times during script
+# execution.
+pip list  || echo "Some error occurred attempting to run command: pip"
+pip3 list || echo "Some error occurred attempting to run command: pip3"
+
+cd "${INSTALL_DIR}"
+find /usr/lib /usr/local $HOME/.local | sort > usr-local-1-before-protobuf.txt
+
+# Starting in 2019-Nov, Python3 version of Scapy is needed for `cd
+# p4c/build ; make check` to succeed.
+sudo pip3 install -U scapy ptf psutil grpcio
+
+# Attempting to install the p4runtime-shell package using the command
+# below gives no errors during installation, but results in a system
+# state where this fails:
+#
+# $ python3
+# >>> from p4.tmp import p4conig_pb2
+# ModuleNotFoundError: No module named 'p4.tmp'
+#
+# And that import must succeed in order for running exercises in the
+# tutorials repository to work.  For now, I will not install
+# p4runtime-shell in this script, until and unless I figure out how to
+# avoid this problem.
+#
+# I believe this happens because before the `sudo pip3 ...' command
+# below is executed, commands above have already created a directory
+# /usr/lib/python3/dist-packages/p4 with a tmp subdirectory and other
+# packages, and then the command below creates a directory
+# /usr/local/lib/python3.8/dist-packages/p4 that does _not_ have a tmp
+# subdirectory.  Perhaps there is a way to run the command below and
+# have it install packages in /usr/lib/python3/dist-packages/p4
+# instead?
+
+#sudo pip3 install git+https://github.com/p4lang/p4runtime-shell.git
+
+set +x
+echo "------------------------------------------------------------"
+
+echo "Installing Mininet - not necessary to run P4 programs, but useful if"
+echo "you want to run tutorials from https://github.com/p4lang/tutorials"
+echo "repository."
+echo "start install mininet:"
+set -x
+date
+
+# Pin to a particular version, so that I know the patch below will
+# continue to apply.  Will likely want to update this to newer
+# versions once or twice a year.
+MININET_COMMIT="5b1b376336e1c6330308e64ba41baac6976b6874"  # 2023-May-28
+git clone https://github.com/mininet/mininet mininet
+cd mininet
+git checkout ${MININET_COMMIT}
+PATCH_DIR="${THIS_SCRIPT_DIR_ABSOLUTE}/patches"
+patch -p1 < "${PATCH_DIR}/mininet-patch-for-2023-jun.patch"
+cd ..
+sudo ./mininet/util/install.sh -nw
+
+set +x
+echo "end install mininet:"
+set -x
+date
+
+cd "${INSTALL_DIR}"
+find /usr/lib /usr/local $HOME/.local | sort > usr-local-7-after-mininet-install.txt
+
+# Check to see which versions of Python-related programs this system
+# has installed when the script is done.
+lsb_release -a
+python -V  || echo "No such command in PATH: python"
+python2 -V || echo "No such command in PATH: python2"
+python3 -V || echo "No such command in PATH: python3"
+pip -V  || echo "No such command in PATH: pip"
+pip2 -V || echo "No such command in PATH: pip2"
+pip3 -V || echo "No such command in PATH: pip3"
+pip list  || echo "Some error occurred attempting to run command: pip"
+pip3 list || echo "Some error occurred attempting to run command: pip3"
+
+set +x
+echo "------------------------------------------------------------"
+echo "Time and disk space used when installation was complete:"
+set -x
+date
+df -h .
+df -BM .
+
+echo "----------------------------------------------------------------------"
+echo "Output of script p4-environment-info.sh"
+echo "----------------------------------------------------------------------"
+"${THIS_SCRIPT_DIR_ABSOLUTE}/p4-environment-info.sh"
+echo "----------------------------------------------------------------------"
+
+cd "${INSTALL_DIR}"
+DETS="install-details"
+mkdir -p "${DETS}"
+mv usr-local-*.txt "${DETS}"
+cd "${DETS}"
